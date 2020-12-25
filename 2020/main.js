5ace4c468f5ae39a6f96a8f26320d1ccbc24b352
Thatguyjs <that.program@gmail.com>
2020-12-25 13:04:25 -0700

    Hard-code shader links

--- 2020/main.js
+++ 2020/main.js
@@ -38,8 +38,8 @@ const Main = {
 		Renderer.resizeCanvas(window.innerWidth / 2, window.innerHeight);
 
 		await Renderer.createProgram('main', {
-			vertex: '/include/gl/color.vert',
-			fragment: '/include/gl/color.frag',
+			vertex: '/Christmas-Code/include/gl/color.vert',
+			fragment: '/Christmas-Code/include/gl/color.frag',
 
 			attributes: {
 				'position': 'aVertexPosition',
