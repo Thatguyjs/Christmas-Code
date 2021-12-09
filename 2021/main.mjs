99e8c5099e06af78090d3a724b0f32e577c77a09
Thatguyjs <that.program@gmail.com>
2021-12-08 22:31:29 -0700

    Day 8 - Add basic movement and camera controls

--- 2021/main.mjs
+++ 2021/main.mjs
@@ -1,4 +1,5 @@
 import Ground from "./ground.mjs";
+import Player from "./player.mjs";
 
 
 const canvas = document.getElementById('cnv');
@@ -12,20 +13,21 @@ const uniforms = {
 	proj_mat: twgl.m4.perspective(45 * Math.PI / 180, canvas.width / canvas.height, 0.01, 100)
 };
 
-twgl.m4.translate(uniforms.mv_mat, [0, -2, -10], uniforms.mv_mat);
-
 await Ground.init(gl, {
-	rows: 20,
-	cols: 20,
+	rows: 50,
+	cols: 50,
 	spacing: 0.5,
 	height_func: (row, col) => {
-		return Math.sin(row) * Math.cos(col) * 0.2;
+		return Math.sin(row) * Math.cos(col ^ (row * 10)) * 0.2 + 0.4;
 	},
 	color_func: (row, col, height) => {
-		height = height * 2.5 + 0.5;
-		return [row / 20, col / 20, height, 1.0];
+		height = height * 0.4 + 0.7;
+		// return [Math.random() * 0.7, Math.random() * 0.1, Math.random() * 0.4, 1.0];
+		return [height, height, height, 1.0];
 	}
 });
+
+Player.init();
 
 gl.clearColor(0.01, 0.01, 0.01, 1);
 gl.enable(gl.DEPTH_TEST);
@@ -37,12 +39,21 @@ function render() {
 	gl.viewport(0, 0, canvas.width, canvas.height);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	Player.update();
+	twgl.m4.translation([-Player.pos.z, 2, -Player.pos.x], uniforms.mv_mat);
+	twgl.m4.rotateY(uniforms.mv_mat, Player.rot.x, uniforms.mv_mat);
+	twgl.m4.rotateX(uniforms.mv_mat, Player.rot.y, uniforms.mv_mat);
+	twgl.m4.inverse(uniforms.mv_mat, uniforms.mv_mat);
+
 	Ground.update(gl);
 	Ground.render(gl, uniforms);
-
-	twgl.m4.rotateY(uniforms.mv_mat, 0.005, uniforms.mv_mat);
 
 	window.requestAnimationFrame(render);
 }
 
 window.requestAnimationFrame(render);
+
+
+window.addEventListener('click', () => {
+	canvas.requestPointerLock();
+});
