038a6e6417eaf21763c232c48bc59db4be77a843
Thatguyjs <that.program@gmail.com>
2022-12-07 15:38:06 -0600

    Add basic rendering code
    Haven't worked on this for a few days, so multiple commits will be added today

--- 2022/main.mjs
+++ 2022/main.mjs
@@ -4,4 +4,25 @@ import { el } from "./util.mjs";
 const cnv = el('canvas');
 const gl = cnv.getContext('webgl2');
 
-console.log("Got context:", gl);
+function resize_canvas() {
+	twgl.resizeCanvasToDisplaySize(cnv);
+	gl.viewport(0, 0, cnv.width, cnv.height);
+}
+
+window.addEventListener('resize', resize_canvas);
+resize_canvas();
+
+
+gl.clearColor(0, 0, 0, 1);
+let prev_time = 0;
+
+function render(time) {
+	const elapsed = time - prev_time; // Elapsed frame time in ms
+	prev_time = time;
+
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	window.requestAnimationFrame(render);
+}
+
+window.requestAnimationFrame(render);
